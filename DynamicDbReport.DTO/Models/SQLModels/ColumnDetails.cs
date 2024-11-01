66b924636b8874a999f180e475ffd23ba2686c6b
a-jouhann <ali.youhannaei@outlook.com>
2024-11-01 14:43:32 +0330

    change column details on import table

--- DynamicDbReport.DTO/Models/SQLModels/ColumnDetails.cs
+++ DynamicDbReport.DTO/Models/SQLModels/ColumnDetails.cs
@@ -3,9 +3,10 @@
 public class ColumnDetails
 {
     public bool Identity { get; set; } = false;
-    public string ColumnName { get; set; }
-    public string ColumnType { get; set; }
+    public string ColumnName { get; set; } = "";
+    public string ColumnType { get; set; } = "";
     public int Length { get; set; } = 0;
     public byte Scale { get; set; } = 0;
     public bool NullableItem { get; set; } = true;
+    public Guid ID { get; set; } = new();
 }
